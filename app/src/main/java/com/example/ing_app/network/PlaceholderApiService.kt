f078739b1579247a7420f83618fdc40ef851cb09
p.sz1 <p.sz1@op.pl>
2020-04-05 20:00:02 +0200

    First attempt to connect app to placeholder API

--- app/src/main/java/com/example/ing_app/network/PlaceholderApiService.kt
+++ app/src/main/java/com/example/ing_app/network/PlaceholderApiService.kt
@@ -1,32 +1,52 @@
 package com.example.ing_app.network
 
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import kotlinx.coroutines.Deferred
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.Converter.Factory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 private const val BASE_URL = "https://jsonplaceholder.typicode.com"
 
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
+    .addConverterFactory(MoshiConverterFactory.create())
     .build()
+
+data class PostProperties(@field:Json(name = "userId") val userId: Int,
+                          @field:Json(name = "id") val id: Int,
+                          @field:Json(name = "title") val title: String,
+                          @field:Json(name = "body") val body: String)
 
 interface PlaceholderApiService{
     @GET("posts")
-    fun getPropertipes():
+    fun getProperties():
             Call<String>
 }
 
 object PlaceholderApi{
-    val retrofitService : PlaceholderApiService by lazy {
+    val retrofitService : PlaceholderApiService by lazy{
         retrofit.create(PlaceholderApiService::class.java)
     }
 }
+
+
+
+/*
+@JsonClass(generateAdapter = true)
+data class Post (
+    //@Json(name = “userId”) annotation defines the JSON key name for serialisation
+    //and the property to set the value on with deserialization.
+    @Json(name = "userId") val userId: Int = -1,
+    val id: Int,
+    val title: String,
+    val body: String
+)
+
+val moshi: Moshi = Moshi.Builder().build()
+val adapter: JsonAdapter<Post> = moshi.adapter(Post::class.java)
+val post = adapter.fromJson(postsJson))*/
--- app/src/main/java/com/example/ing_app/overview/OverviewViewModel.kt
+++
@@ -1,2 +0,0 @@
-package com.example.ing_app.overview
-
