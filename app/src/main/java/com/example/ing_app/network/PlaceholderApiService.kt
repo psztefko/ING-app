08c98afb7acc52686d77659720a2c5fc09adbe4d
p.sz1 <p.sz1@op.pl>
2020-04-05 20:02:12 +0200

    First attempt to connect app to placeholder API

--- app/src/main/java/com/example/ing_app/network/PlaceholderApiService.kt
+++ app/src/main/java/com/example/ing_app/network/PlaceholderApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-
+//
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com"
 
