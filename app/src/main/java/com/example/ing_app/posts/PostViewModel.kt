e88e28a04ab863a26e7b32d458e6f86eb6357d76
adasumizox <darkdan099@gmail.com>
2020-04-12 18:38:39 +0200

    added basic fragments and viewModels and postRepository need to add layouts and inflate view in post fragment to check if it's working

---
+++ app/src/main/java/com/example/ing_app/posts/PostViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.ing_app.posts
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ing_app.repository.PostRepository
+import kotlinx.coroutines.launch
+
+class PostViewModel(private val postRepository: PostRepository) : ViewModel() {
+    fun getPosts() {
+        viewModelScope.launch {
+            val apiResult = postRepository.getAllPosts()
+        }
+    }
+}
