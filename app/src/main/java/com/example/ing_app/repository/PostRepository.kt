b85b50d7707a805f5a934b9d21776a974301a5f0
adasumizox <darkdan099@gmail.com>
2020-04-26 00:10:09 +0200

    Added Exceptions and assignment of PostList to LiveData. Result Built-in type was not enough

--- app/src/main/java/com/example/ing_app/repository/PostRepository.kt
+++ app/src/main/java/com/example/ing_app/repository/PostRepository.kt
@@ -1,5 +1,8 @@
 package com.example.ing_app.repository
 
+import com.example.ing_app.common.Result
+import com.example.ing_app.common.exception.CancelledFetchDataException
+import com.example.ing_app.common.exception.NetworkException
 import com.example.ing_app.domain.Post
 import com.example.ing_app.network.Comment.CommentService
 import com.example.ing_app.network.Post.PostService
@@ -11,21 +14,29 @@ import timber.log.Timber
 class PostRepository (private val postService: PostService,
                       private val userService: UserService,
                       private val commentService: CommentService) {
-    suspend fun getPosts() {
-        var result: Result<List<Post>>
+    suspend fun getPosts() : Result<List<Post>> {
+        var result: Result<List<Post>> = Result.success(emptyList())
 
         withContext(Dispatchers.IO) {
-            val request = postService.getPosts()
-            val response = request.await()
-            Timber.d("onPostsReceived {$request}")
+            try {
+                val request = postService.getPosts()
+                val response = request.await()
+                Timber.d("onPostsReceived {$request}")
 
-            request.let {
-                if (it.isCompleted) {
-                    result = Result.success(response)
+                request.let {
+                    if (it.isCompleted) {
+                        result = Result.success(response)
+                    }
+                    else if (it.isCancelled) {
+                        result = Result.failure(CancelledFetchDataException())
+                    }
                 }
+            } catch (ex: Throwable) {
+                result = Result.failure(NetworkException())
+                Timber.d("onPostReceived NetworkException")
             }
         }
-        // return result
+        return result
     }
     suspend fun getUserFromPost(postId:Int) {
         withContext(Dispatchers.IO) {
--- app/src/main/java/com/example/ing_app/ui/posts/PostViewModel.kt
+++ app/src/main/java/com/example/ing_app/ui/posts/PostViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.ing_app.ui.posts
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.ing_app.common.Result
+import com.example.ing_app.common.ResultType
+import com.example.ing_app.domain.Post
 import com.example.ing_app.repository.PostRepository
 import kotlinx.coroutines.launch
+import timber.log.Timber
+
 
 class PostViewModel(private val postRepository: PostRepository) : ViewModel() {
-
+    val posts: MutableLiveData<List<Post>> = MutableLiveData()
 
     init {
         getPosts()
@@ -15,6 +21,17 @@ class PostViewModel(private val postRepository: PostRepository) : ViewModel() {
     fun getPosts() {
         viewModelScope.launch {
             val apiResult = postRepository.getPosts()
+            updatePosts(apiResult)
         }
+    }
+
+    private fun updatePosts(result: Result<List<Post>>) {
+        if (isResultSuccess(result.resultType)) {
+            posts.postValue(result.data)
+        }
+    }
+
+    private fun isResultSuccess(resultType: ResultType): Boolean {
+        return resultType == ResultType.SUCCESS
     }
 }
