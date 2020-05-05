e68ca81eb800f5fe280a5f9e482bc599e92bef8c
adasumizox <darkdan099@gmail.com>
2020-05-05 10:47:10 +0200

    Showing post and comments work

--- app/src/main/java/com/example/ing_app/ui/user/UserViewModel.kt
+++ app/src/main/java/com/example/ing_app/ui/user/UserViewModel.kt
@@ -4,16 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.ing_app.common.ResultType
+import com.example.ing_app.common.Result
 import com.example.ing_app.domain.User
 import com.example.ing_app.repository.UserRepository
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class UserViewModel (private val userKey: Int = 0,
                      private val userRepository: UserRepository): ViewModel(){
     private val _user: MutableLiveData<User> = MutableLiveData()
     val user: LiveData<User>
         get() = _user
+    private val _isErrorLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val isErrorLiveData: LiveData<Boolean>
+        get() = _isErrorLiveData
 
     init {
         getUser()
@@ -21,9 +25,23 @@ class UserViewModel (private val userKey: Int = 0,
 
     private fun getUser() {
         viewModelScope.launch {
-            Timber.d("getUser")
             val apiResult = userRepository.getUserFromPost(userKey)
-            Timber.d(apiResult.data!!.name)
+            updateUser(apiResult)
         }
     }
+
+    private fun updateUser(result: Result<User>) {
+        if (isResultSuccess(result.resultType)) {
+            _user.postValue(result.data)
+        } else {
+            onResultError()
+        }
+
+    }
+
+    private fun isResultSuccess(resultType: ResultType): Boolean {
+        return resultType == ResultType.SUCCESS
+    }
+
+    private fun onResultError() = _isErrorLiveData.postValue(true)
 }
