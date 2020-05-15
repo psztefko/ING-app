5adca7b847d7dddcf6cbd4efa4a3ca20e0878459
adasumizox <darkdan099@gmail.com>
2020-05-15 22:08:16 +0200

    picasso works perfectly

--- app/src/main/java/com/example/ing_app/ui/images/BindingAdapter.kt
+++ app/src/main/java/com/example/ing_app/ui/images/BindingAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.ing_app.ui.images
 
 import android.widget.ImageView
-import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.ing_app.R
 import com.example.ing_app.domain.Photo
+import com.squareup.picasso.Picasso
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Photo>?) {
@@ -18,12 +16,11 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Photo>?) {
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
-            .load(imgUri)
-            .apply(RequestOptions()
-                .placeholder(R.drawable.loading_animation)
-                .error(R.drawable.ic_broken_image_black_24dp))
+        Picasso.get()
+            .load(imgUrl)
+            .resize(150,150)
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_broken_image_black_24dp)
             .into(imgView)
     }
 }
