8dfbd8f80331d2e6ac0c8142ceb203c59ea804bc
adasumizox <darkdan099@gmail.com>
2020-05-28 19:51:50 +0200

    Maybe not best code quality but menu and changing icon works

--- app/src/main/java/com/example/ing_app/ui/posts/PostFragment.kt
+++ app/src/main/java/com/example/ing_app/ui/posts/PostFragment.kt
@@ -1,14 +1,12 @@
 package com.example.ing_app.ui.posts
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.ing_app.R
 import com.example.ing_app.databinding.FragmentPostBinding
 import kotlinx.android.synthetic.main.fragment_post.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -16,6 +14,8 @@ import timber.log.Timber
 
 class PostFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     private val viewModel: PostViewModel by sharedViewModel()
+    // Propably it can be done in databinding
+    private var isDark: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -59,11 +59,38 @@ class PostFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // I am not sure if it's good idea
+        isDark = !isDark
+        setHasOptionsMenu(false)
+        setHasOptionsMenu(true)
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        if(!isDark) {
+            menu.findItem(R.id.theme_icon_menu)
+                .setIcon(R.drawable.ic_brightness_low_black_24dp)
+                .setTitle(R.string.change_dark_theme)
+        } else {
+            menu.findItem(R.id.theme_icon_menu)
+                .setIcon(R.drawable.ic_brightness_high_black_24dp)
+                .setTitle(R.string.change_light_theme)
+        }
+        super.onPrepareOptionsMenu(menu)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         swipeRefreshLayout.setOnRefreshListener() {
             Timber.d("onRefreshListener")
             onRefresh()
         }
+        setHasOptionsMenu(true)
     }
 
     override fun onRefresh() {
