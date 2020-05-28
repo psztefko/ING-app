b9b39aaf69eab2bcfe22c78c69509781ae0dc5a9
adasumizox <darkdan099@gmail.com>
2020-05-28 22:12:04 +0200

    fixed dark theme

--- app/src/main/java/com/example/ing_app/ui/posts/PostFragment.kt
+++ app/src/main/java/com/example/ing_app/ui/posts/PostFragment.kt
@@ -6,16 +6,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.example.ing_app.R
 import com.example.ing_app.databinding.FragmentPostBinding
 import kotlinx.android.synthetic.main.fragment_post.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import timber.log.Timber
 
 class PostFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     private val viewModel: PostViewModel by sharedViewModel()
-    // Propably it can be done in databinding
-    private var isDark: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,40 +53,6 @@ class PostFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
             }
         })
         return binding.root
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // I am not sure if it's good idea
-        isDark = !isDark
-        setHasOptionsMenu(false)
-        setHasOptionsMenu(true)
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        if(!isDark) {
-            menu.findItem(R.id.theme_icon_menu)
-                .setIcon(R.drawable.ic_brightness_low_black_24dp)
-                .setTitle(R.string.change_dark_theme)
-        } else {
-            menu.findItem(R.id.theme_icon_menu)
-                .setIcon(R.drawable.ic_brightness_high_black_24dp)
-                .setTitle(R.string.change_light_theme)
-        }
-        super.onPrepareOptionsMenu(menu)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        swipeRefreshLayout.setOnRefreshListener() {
-            Timber.d("onRefreshListener")
-            onRefresh()
-        }
-        setHasOptionsMenu(true)
     }
 
     override fun onRefresh() {
